c8b6a11f2be8768067f7dd284363874137f6b61a
akagrecha <akagrecha@gmail.com>
2024-09-01 22:33:34 +0000

    patience in yaml; out of space

--- train.sh
+++ train.sh
@@ -2,16 +2,20 @@
 
 trainfile="data/halueval_dialogue.json"
 # trainfile=data/truthfulQA/truthful_qa.json
-regression=skill
-mode=pewcrowdae
+regression=hardlabel
+# mode=pewcrowdae
 # mode=compression
 task=halueval
+mode=gt
+# task=truthfulqa
 
 # trainfile="data/artificial.json"
 # trainfile="data/wikibio_crosscheck_gpt3.json"
 # expdir=exp/pewcrowd_gpt2_mse_direct_crowdlayer_constrained_Xtcondition
-expdir=exp/pewcrowd_gpt2_mse_direct_crowdlayer_${mode}_${regression}_${task}_5beluga
+# expdir=exp/pewcrowd_gpt2_mse_direct_crowdlayer_${mode}_${regression}_${task}_5beluga
 # expdir=exp/worker_compression_encoder_decoder_5beluga
+# expdir=exp/truthfulqa/gt_brian_sept_1
+expdir=exp/halueval/gt_brian_sept_1
 mkdir -p $expdir
 
 
@@ -34,30 +38,30 @@ mkdir -p $expdir
 #     --split 0.9 \
 #     --target_nllms 7 \
 
-python train_nn.py \
-    --model_path gpt2 \
-    --batch_size 8 \
-    --learning_rate 1e-4 \
-    --gradient_accumulation_steps 1 \
-    --num_train_epochs 10 \
-    --num_warmup_steps 0.03 \
-    --weight_decay 0.0 \
-    --lr_scheduler_type cosine \
-    --outputdir $expdir \
-    --logfile $expdir/log.txt \
-    --log_interval 100 \
-    --train_data_path $trainfile \
-    --evidence_llm "llama3,beluga,mistral,zephyr,starling,openorca,beluga,beluga,beluga,beluga" \
-    --regression $regression \
-    --mode $mode \
-    --split 0.1 \
-    --freeze_epoch 200 \
-    --reg_factor 0.5 \
-    --target_nllms 7 \
-    --encdecpath exp/worker_compression_encoder_decoder_CE_5beluga/checkpoint.49/pytorch_model.pt \
-    # --evidence_llm "mistral,llama2,vicuna,beluga,starling,openorca,gpt3" \
-    # "system_0,system_1,system_2,system_3,system_4" \
-    #  "llama3,beluga,mistral,zephyr,starling,openorca,dolphin,mistral1,hermes2,hermes25" \
+# python train_nn.py \
+#     --model_path gpt2 \
+#     --batch_size 8 \
+#     --learning_rate 1e-4 \
+#     --gradient_accumulation_steps 1 \
+#     --num_train_epochs 10 \
+#     --num_warmup_steps 0.03 \
+#     --weight_decay 0.0 \
+#     --lr_scheduler_type cosine \
+#     --outputdir $expdir \
+#     --logfile $expdir/log.txt \
+#     --log_interval 100 \
+#     --train_data_path $trainfile \
+#     --evidence_llm "llama3,beluga,mistral,zephyr,starling,openorca,beluga,beluga,beluga,beluga" \
+#     --regression $regression \
+#     --mode $mode \
+#     --split 0.1 \
+#     --freeze_epoch 200 \
+#     --reg_factor 0.5 \
+#     --target_nllms 7 \
+#     --encdecpath exp/worker_compression_encoder_decoder_CE_5beluga/checkpoint.49/pytorch_model.pt \
+#     # --evidence_llm "mistral,llama2,vicuna,beluga,starling,openorca,gpt3" \
+#     # "system_0,system_1,system_2,system_3,system_4" \
+#     #  "llama3,beluga,mistral,zephyr,starling,openorca,dolphin,mistral1,hermes2,hermes25" \
 
 
 # python train_nn.py \
@@ -85,20 +89,20 @@ python train_nn.py \
 # expdir=${expdir}_2
 # 
 # mkdir -p $expdir
-# python train_nn.py \
-#     --model_path gpt2 \
-#     --batch_size 32 \
-#     --learning_rate 1e-4 \
-#     --gradient_accumulation_steps 1 \
-#     --num_train_epochs 10 \
-#     --num_warmup_steps 0.03 \
-#     --weight_decay 0.0 \
-#     --lr_scheduler_type cosine \
-#     --outputdir $expdir \
-#     --logfile $expdir/log.txt \
-#     --log_interval 200 \
-#     --train_data_path $trainfile \
-#     --evidence_llm "llama3,beluga,mistral,zephyr,starling,openorca,dolphin,mistral1,hermes2,hermes25" \
-#     --regression $regression \
-#     --mode $mode \
-#     --split 0.2 \
+python train_nn.py \
+    --model_path gpt2 \
+    --batch_size 16 \
+    --learning_rate 1e-4 \
+    --gradient_accumulation_steps 1 \
+    --num_train_epochs 10 \
+    --num_warmup_steps 0.03 \
+    --weight_decay 0.0 \
+    --lr_scheduler_type cosine \
+    --outputdir $expdir \
+    --logfile $expdir/log.txt \
+    --log_interval 200 \
+    --train_data_path $trainfile \
+    --evidence_llm "llama3,beluga,mistral,zephyr,starling,openorca,dolphin,mistral1,hermes2,hermes25" \
+    --regression $regression \
+    --mode $mode \
+    --split 0.1 \
