3751a4fc393e4097502638706834fa2adca31d93
akagrecha <akagrecha@gmail.com>
2024-09-10 04:19:00 +0000

    refactor commands, loaders; embed exps

--- worker_agg/__init__.py
+++ worker_agg/__init__.py
@@ -4,9 +4,8 @@ from .logit_policies import EMGaussian, EM_GMM, AvgSSLPreds, Averaging, AvgSSLPr
 from .lm_policies import LMGroundTruth, LMMajVote, CrowdLayerLM, AvgSSLPredsLM
 from .lm_policies import AvgSSLPredsSepLMs, PEWNoSSLSepLMs, PEWNoSSL
 from .data_loaders import HaluQABinary
-from .data_loaders import HaluDialEmbed, NoContextData
+from .data_loaders import EmbedData, NoContextData, FullContextData
 from .data_loaders import SynLogisticData, SynTwoLayerMLPData
-from .data_loaders import HaluDialLM
 from .utils import TwoLayerMLP, train_neural_net, train_neural_net_with_loaders
 from .utils import CustomDataset
 from .lm_utils import LMplusOneLayer, FinetuneLM, CombinedModel
